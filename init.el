7b798dccd2da78f53b39dc8d702ec55163a4f316
Marcel Bornancin <marcel.bornancin@nubank.com.br>
2025-11-14 17:20:55 -0300

    Enable tramp module

--- init.el
+++ init.el
@@ -70,6 +70,7 @@
        dired             ; making dired pretty [functional]
        electric          ; smarter, keyword-based electric-indent
        ;;ibuffer         ; interactive buffer management
+       tramp             ; remote files at your arthritic fingertips
        (undo +tree)      ; persistent, smarter undo for your inevitable mistakes
        vc                ; version-control and Emacs, sitting in a tree
 
