f9dd23ec10e2b7da1d537049f0be2afaf0816864
Marcel Bornancin <marcel.bornancin@nubank.com.br>
2025-12-22 13:19:53 -0300

    Move org-roam config to init-org.el

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -61,5 +61,48 @@
         ("on" "Project notes" entry #'+org-capture-central-project-notes-file "* %U %?\n %i\n %a" :heading "Notes" :clock-resume t :kill-buffer t)
         ("oc" "Project changelog" entry #'+org-capture-central-project-changelog-file "* %U %?\n %i\n %a" :heading "Changelog" :clock-resume t)))
 
+;; org-roam stuff
+
+(when (modulep! :lang org +roam)
+  (map! :leader
+        :desc "Find node"        "n r n" #'org-roam-node-find
+        :desc "Capture to node"  "n r c" #'org-roam-capture
+        :desc "Add alias"        "n r a" #'org-roam-alias-add
+        :desc "Remove alias"     "n r A" #'org-roam-alias-remove
+        :desc "Add tag"          "n r t" #'org-roam-tag-add
+        :desc "Remove tag"       "n r T" #'org-roam-tag-remove
+        :desc "Open random node" "n r #" #'org-roam-node-random)
+
+  (setq org-roam-directory (expand-file-name "roam/" org-directory)
+        ;; override default template to add created/modified/filetags props
+        org-roam-capture-templates
+        `(("d" "default" plain "\n* TODO roam entry: ${title}%?"
+           :target (file+head "%<%Y%m%d%H%M%S>-${slug}.org"
+                              ,(concat "#+title: ${title}\n"
+                                       "#+created:       %U\n"
+                                       "#+last_modified: %U\n"
+                                       "#+filetags:\n"))
+           :unnarrowed t)))
+
+  ;; Update "last_modified" date when saving buffer
+  ;; https://org-roam.discourse.group/t/update-a-field-last-modified-at-save/321
+  (defun mabo3n/org-roam-set-time-stamp-vars ()
+    "Set time-stamp variables to auto update last_modified property."
+    (when (derived-mode-p 'org-mode)
+      (require 'time-stamp)
+      (setq-local time-stamp-active t
+                  time-stamp-line-limit 24
+                  time-stamp-start "#\\+last_modified:[ ]*"
+                  time-stamp-end "$"
+                  time-stamp-format "\[%Y-%m-%d %3a %H:%M\]")))
+
+  (defun mabo3n/org-roam-timestamp-on-save ()
+    "Call `time-stamp' function if in `org-mode'."
+    (when (derived-mode-p 'org-mode)
+      (time-stamp)))
+  (add-hook 'org-mode-hook #'mabo3n/org-roam-set-time-stamp-vars)
+  (add-hook 'before-save-hook #'mabo3n/org-roam-timestamp-on-save))
+
+
 (provide 'init-org)
 ;;; init-org.el ends here
